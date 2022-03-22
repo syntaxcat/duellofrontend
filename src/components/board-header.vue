af18dd28db15ee0a0c543d4798d88a39a0adfdf6
shemer binner <shemer146@gmail.com>
2022-03-22 23:57:28 +0200

    board-header style

--- src/components/board-header.vue
+++ src/components/board-header.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="board-header">
-    <div class="right-side">
+    <div class="left-side">
       <div class="board-name">
         <input
           type="text"
@@ -8,16 +8,21 @@
           v-model="boardName"
           v-if="isEditing"
           @blur="isEditing = !isEditing"
+          :style="width"
         />
-        <span v-if="!isEditing" @click="editMode">{{ boardName }}</span>
+        <div v-if="!isEditing" @click="editMode">{{ boardName }}</div>
       </div>
 
       <button @click="setFavorite">
         <img :class="isFavorite" src="../assets/icons/bx-star.svg" alt="star" />
       </button>
+      <div class="divider">
       |
+      </div>
       <button>Sprint</button>
+      <div class="divider">
       |
+      </div>
       <!-- <section class="members">
       <div class="member" v-for="member in members" :key="member._id">
         <img :src="member.imgUrl" alt="member" />
@@ -25,7 +30,8 @@
       <button>Invite</button>
     </section> -->
     </div>
-    <div class="left-side">
+   
+    <div class="right-side">
       <button>
           <img src="../assets/icons/bx-filter.svg" alt="">
         <span>Filter</span>
@@ -60,6 +66,11 @@ export default {
     isFavorite() {
       return this.isFull ? "full-star" : "empty-star";
     },
+    width(){
+      return {
+        'max-width': `calc(10% - em(${this.boardName.length*2}px))`
+      }
+    }
   },
 };
 </script>
