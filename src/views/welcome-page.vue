d0821a927f98895070de1ca2df51d802fb713ac8
Ron Shitrit <ronshitrit456@gmail.com>
2022-03-22 19:17:03 +0200

    'scss added

--- src/views/welcome-page.vue
+++ src/views/welcome-page.vue
@@ -1,13 +1,23 @@
 <template>
-  <section class="welcome-page">
-      <div class="welcome-header">
-          <div>
-              <div class="welcome-logo">Trello</div>
-              <button class="welcome-btn">Sighn up</button>
-              <button class="welcome-btn">Sighn up</button>
-          </div>
-      </div>
-  </section>
+    <section class="welcome-page">
+        <div class="welcome-header">
+            <div class="welcome-logo">Trello</div>
+            <button class="welcome-btn">Sign up</button>
+            <button class="welcome-btn">Sign in</button>
+        </div>
+        <div class="welcome-signin">
+            <h1>Trello helps teams move work forward.</h1>
+            <p>
+                Collaborate, manage projects, and reach new productivity peaks.
+                From high rises to the home office, the way your team works is unique—accomplish
+                it all with Trello.
+            </p>
+            <form>
+                <input type="text" />
+                <input type="submit" value="Sign up-it's free!" />
+            </form>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -17,5 +27,4 @@ export default {
 </script>
 
 <style>
-
 </style>
