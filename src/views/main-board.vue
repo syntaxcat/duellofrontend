4b67c2ed77f66fb3e9a178e7007c92d076afe917
Michaela Bereza <michaelabereza@icloud.com>
2022-03-22 20:18:10 +0200

    another fix for main header§

--- src/views/main-board.vue
+++ src/views/main-board.vue
@@ -1,20 +1,17 @@
 <template>
   <section class="main-board">
-      <boardHeader />
-
-
     <mainHeader />
+    <boardHeader />
+
     <section class="groups-container"></section>
   </section>
 </template>
 
 <script>
-import boardHeader from "../components/board-header.vue"
-import mainHeader from "../components/main-header.vue";
+  import boardHeader from "../components/board-header.vue";
+  import mainHeader from "../components/main-header.vue";
 
-export default {
-
-  components:{boardHeader,mainHeader}
-
-}
+  export default {
+    components: { boardHeader, mainHeader },
+  };
 </script>
