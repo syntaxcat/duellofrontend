f1a636a2b370cba2f467ab364621fbd5151610fc
shemer binner <shemer146@gmail.com>
2022-03-22 21:57:35 +0200

    more changes in board-header

--- src/components/board-header.vue
+++ src/components/board-header.vue
@@ -1,11 +1,39 @@
 <template>
   <section class="board-header">
-    <div class="board-header-btn">
-      <input type="text" v-model="boardName" v-if="isEditing" @blur="isEditing = !isEditing"
-      autofocus />
-      <span v-if="!isEditing" @click="editMode">{{
-        boardName
-      }}</span>
+    <div class="right-side">
+      <div class="board-name">
+        <input
+          type="text"
+          ref="myInput"
+          v-model="boardName"
+          v-if="isEditing"
+          @blur="isEditing = !isEditing"
+        />
+        <span v-if="!isEditing" @click="editMode">{{ boardName }}</span>
+      </div>
+
+      <button @click="setFavorite">
+        <img :class="isFavorite" src="../assets/imgs/bx-star.svg" alt="star" />
+      </button>
+      |
+      <button>Sprint</button>
+      |
+      <!-- <section class="members">
+      <div class="member" v-for="member in members" :key="member._id">
+        <img :src="member.imgUrl" alt="member" />
+      </div>
+      <button>Invite</button>
+    </section> -->
+    </div>
+    <div class="left-side">
+      <button>
+          <img src="../assets/imgs/bx-filter.svg" alt="">
+        <span>Filter</span>
+      </button>
+      <button>
+          <img src="../assets/imgs/more-horizontal.svg" alt="">
+        <span>Show menu</span>
+      </button>
     </div>
   </section>
 </template>
@@ -15,20 +43,24 @@ export default {
   data() {
     return {
       boardName: "Duello",
-      isEditing: false,
+      isEditing: true,
+      isFull: false,
     };
   },
-  methods:{
-      editMode(){
-          this.isEditing = !this.isEditing
-        //   this.$refs.myInput.focus()
-      }
+  methods: {
+    editMode() {
+      this.isEditing = !this.isEditing;
+      this.$nextTick(() => this.$refs.myInput.focus());
+    },
+    setFavorite(boardId) {
+      this.isFull = !this.isFull;
+    },
   },
-  computed:{
-      focus(){
-          return this.isEditing ? 'autofocus' : ''
-      }
-  }
+  computed: {
+    isFavorite() {
+      return this.isFull ? "full-star" : "empty-star";
+    },
+  },
 };
 </script>
 
