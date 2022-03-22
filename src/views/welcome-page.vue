7fa81b18eb5a7f6da898d95dc66bcb6ea05464d5
Ron Shitrit <ronshitrit456@gmail.com>
2022-03-22 18:59:27 +0200

    welcome page added

---
+++ src/views/welcome-page.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="welcome-page">
+      <div class="welcome-header">
+          <div>
+              <button class="welcome-btn">Sighn up</button>
+              <button class="welcome-btn">Sighn up</button>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
