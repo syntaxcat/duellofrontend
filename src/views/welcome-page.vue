5db68d287fd16cbdb008d783cf018a490a701705
Ron Shitrit <ronshitrit456@gmail.com>
2022-03-22 22:04:28 +0200

    welcome page update

--- src/views/welcome-page.vue
+++ src/views/welcome-page.vue
@@ -1,25 +1,27 @@
 <template>
     <section class="welcome-page">
         <div class="welcome-header">
-            <div class="welcome-logo">Trello</div>
+            <div class="welcome-logo">Duello</div>
+            <div class="welcome-btn-container">
+            <button class="welcome-btn">Log in</button>
             <button class="welcome-btn">Sign up</button>
-            <button class="welcome-btn">Sign in</button>
             </div>
-        <div class="welcome-body">
-        <div class="welcome-signin">
-            <h1>Trello helps teams move work forward.</h1>
-            <p>
-                Collaborate, manage projects, and reach new productivity peaks.
-                From high rises to the home office, the way your team works is unique—accomplish
-                it all with Trello.
-            </p>
-            <form>
-                <input type="text" />
-                <input type="submit" value="Sign up-it's free!" />
-            </form>
         </div>
-            <img class="welcome-img" src="../assets/imgs/welcome-img.png"/>
+        <div class="welcome-body">
+            <div class="welcome-signin">
+                <h1>Duello helps teams move work forward.</h1>
+                <p>
+                    Collaborate, manage projects, and reach new productivity peaks.
+                    From high rises to the home office, the way your team works is unique—accomplish
+                    it all with Trello.
+                </p>
+                <form>
+                    <!-- <input type="text" /> -->
+                    <input class="welcome-btn" type="submit" value="Check us out - it's free!" />
+                </form>
             </div>
+            <img class="welcome-img" src="../assets/imgs/welcome-img.png" />
+        </div>
     </section>
 </template>
 
