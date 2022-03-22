f7cc92c222264f3923dd6ec49afa79d071ce770b
Ron Shitrit <ronshitrit456@gmail.com>
2022-03-22 19:06:38 +0200

    'imgs moved to new folder in assets

--- src/views/welcome-page.vue
+++ src/views/welcome-page.vue
@@ -2,6 +2,7 @@
   <section class="welcome-page">
       <div class="welcome-header">
           <div>
+              <div class="welcome-logo">Trello</div>
               <button class="welcome-btn">Sighn up</button>
               <button class="welcome-btn">Sighn up</button>
           </div>
