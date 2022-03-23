11132656a8720a65438907d836b9483863a4914f
Michaela Bereza <michaelabereza@icloud.com>
2022-03-23 16:08:09 +0200

    changes to the structure

--- src/store/modules/board.store.js
+++ src/store/modules/board.store.js
@@ -12,53 +12,8 @@ export const boardStore = {
   },
   mutations: {
     setBoard(state, { board }) {
-      console.log("board", board);
       state.board = board;
       state.boardGroups = board.groups;
-    },
-    addGroup(state, { newGroup }) {
-      state.boardGroups.push(newGroup);
-    },
-    removeGroup(state, { groupId }) {
-      const idx = state.boardGroups.findIndex((group) => group.id === groupId);
-      if (idx !== -1) return;
-      state.boardGroups.splice(idx, 1);
-    },
-  },
-  actions: {
-    async loadBoard({ commit }) {
-      try {
-        const board = await boardService.query("b101");
-        commit({ type: "setBoard", board });
-        return board;
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    async addGroup({ state, commit }, { title }) {
-      try {
-        const newBoard = JSON.parse(JSON.stringify(state.board));
-        const newGroup = await boardService.addGroup(title, newBoard);
-        commit({ type: "addGroup", newGroup });
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    async removeGroup({ state, commit }, { groupId }) {
-      try {
-        await JSON.parse(JSON.stringify(state.board));
-        commit({ type: "removeGroup", groupId });
-      } catch (err) {
-        console.log(err);
-      }
-    },
-  },
-
-  mutations: {
-    setBoard(state, { board }) {
-      // console.log('board', board)
-      state.board = board;
-      // state.boardGroups = board.groups
     },
     setBoards(state, { boards }) {
       state.boards = boards;
@@ -71,6 +26,11 @@ export const boardStore = {
         (grp) => grp.id === updatedGroup.id
       );
       state.board.groups.splice(idx, 1, updatedGroup);
+    },
+    removeGroup(state, { groupId }) {
+      const idx = state.boardGroups.findIndex((group) => group.id === groupId);
+      if (idx !== -1) return;
+      state.boardGroups.splice(idx, 1);
     },
   },
   actions: {
