03f4b239d500084a2ea2772216f81da89ad5451e
Michaela Bereza <michaelabereza@icloud.com>
2022-03-23 23:39:44 +0200

    making textarea instead of input

--- src/components/task-preview.vue
+++ src/components/task-preview.vue
@@ -1,42 +1,57 @@
 <template>
   <section class="task-preview">
     <!-- {{ task.title }} -->
-    <input
-          type="text"
-          ref="myInput"
-          v-model="taskToEdit.title"
-          v-if="isEditing"
-          @blur="edit"
-        />
-        <div v-if="!isEditing" @click="edit">{{ taskToEdit.title }}</div>
+    <textarea
+      type="text"
+      :class="{ isEditing: isEditing }"
+      v-model="taskToEdit.title"
+      ref="textarea"
+      @click="isEditing = true"
+      @blur="edit"
+    ></textarea>
   </section>
 </template>
 
 <script>
-export default {
-  props: {
-    task: {
-      type: Object,
-      required: true,
+  export default {
+    props: {
+      task: {
+        type: Object,
+        required: true,
+      },
     },
-  },
-  data() {
-    return {
-      isEditing: false,
-        taskToEdit: {...this.task},
-    }
-  },
-  methods:{
-    edit() {
-        this.isEditing = !this.isEditing;
-        if (this.isEditing) this.$nextTick(() => this.$refs.myInput.focus());
+    data() {
+      return {
+        isEditing: false,
+        taskToEdit: { ...this.task },
+      };
+    },
+    methods: {
+      edit() {
+        this.isEditing = false;
 
         if (this.taskToEdit.title !== this.task.title)
           this.$emit("editTask", this.taskToEdit);
-      }
-  },
-  computed:{}
-};
+      },
+      updateHeigh() {
+        this.$refs.textarea.style.height = 0;
+        this.$refs.textarea.style.height =
+          this.$refs.textarea.scrollHeight + "px";
+      },
+    },
+    mounted() {
+      this.updateHeigh();
+    },
+    watch: {
+      taskToEdit: {
+        handler(val) {
+          this.updateHeigh();
+        },
+        deep: true,
+      },
+    },
+    computed: {},
+  };
 </script>
 
 <style></style>
