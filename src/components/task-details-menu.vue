a64632a45d59474e13dddb03532a9a3e6f4fd001
Michaela Bereza <michaelabereza@icloud.com>
2022-03-25 17:47:48 +0300

    buttons in menu details

--- src/components/task-details-menu.vue
+++ src/components/task-details-menu.vue
@@ -1,74 +1,102 @@
 <template>
   <section class="task-details-menu">
-      
-      <div class="task-menu">
+
+    <div class="task-menu">
+      <div class="task-menu-group">
         <h2>Suggested</h2>
         <button @click="joinTask">
-          <img src="../assets/icons/bx-user.svg" alt="user" />
+          <img
+            src="../assets/icons/bx-user.svg"
+            alt="user"
+          />
           Join
         </button>
+      </div>
 
+      <div class="task-menu-group">
         <h2>Add to card</h2>
         <button @click="openModal('member-cmp')">
-          <img src="../assets/icons/users.svg" alt="users" />
+          <img
+            src="../assets/icons/users.svg"
+            alt="users"
+          />
           Members
         </button>
         <button @click="openModal('label-cmp')">
-          <img src="../assets/icons/tag.svg" alt="label" />
+          <img
+            src="../assets/icons/tag.svg"
+            alt="label"
+          />
           Labels
         </button>
         <button @click="openModal('checklist-cmp')">
-          <img src="../assets/icons/check-square.svg" alt="checklist" />
+          <img
+            src="../assets/icons/check-square.svg"
+            alt="checklist"
+          />
           Checklist
         </button>
         <button @click="openModal('calendar-cmp')">
-          <img src="../assets/icons/bx-time-five.svg" alt="clock" />
+          <img
+            src="../assets/icons/bx-time-five.svg"
+            alt="clock"
+          />
           Dates
         </button>
         <button @click="openModal('attachment-cmp')">
-          <img src="../assets/icons/paperclip.svg" alt="clip" />
+          <img
+            src="../assets/icons/paperclip.svg"
+            alt="clip"
+          />
           Attachment
         </button>
         <button @click="openModal('cover-cmp')">
-          <img src="../assets/icons/bxs-dock-bottom.svg" alt="cover" />
+          <img
+            src="../assets/icons/bxs-dock-bottom.svg"
+            alt="cover"
+          />
           Cover
         </button>
       </div>
-      <div class="actions-menu">
+
+      <div class="task-menu-group">
         <h2>Actions</h2>
         <button @click="copyTask">
-          <img src="../assets/icons/bx-copy.svg" alt="duplicate" />
+          <img
+            src="../assets/icons/bx-copy.svg"
+            alt="duplicate"
+          />
           Copy
         </button>
         <button @click="archiveTask">
-          <img src="../assets/icons/bx-box.svg" alt="archive" />
+          <img
+            src="../assets/icons/bx-box.svg"
+            alt="archive"
+          />
           Archive
         </button>
       </div>
- 
+    </div>
+
   </section>
 </template>
 
 <script>
 // import { taskService } from "../services/task.service";
 
-  export default {
-      props:{
-          
-      },
-    data() {
-      return {
-         
-      };
+export default {
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {
+    openModal(type) {
+      this.$emit("openModal", type);
     },
-    methods: {
-      openModal(type){
-        this.$emit('openModal', type)
-      },
     //   copyTask() {},
     //   archiveTask() {},
-    },
-    computed: {},
-    components: {},
-  };
+  },
+  computed: {},
+  components: {},
+};
 </script>
