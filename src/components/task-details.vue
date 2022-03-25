02fe06339b64122aed0995c46a08294ae46507f8
shemer binner <shemer146@gmail.com>
2022-03-25 12:25:15 +0300

    styling task-details

--- src/components/task-details.vue
+++ src/components/task-details.vue
@@ -4,7 +4,9 @@
       <img src="../assets/icons/x.svg" alt="close" />
     </button>
     <div class="task-header-container">
-      <!-- <img src="../assets/icons/" alt=""> -->
+      <div class="cover-container">
+        <!-- <img src="../assets/imgs/background.jpg" alt="" /> -->
+      </div>
       <input type="text" v-model="taskToEdit.title" />
       <div class="info-in-group">
         <p>in list {{ group.title }}</p>
@@ -12,106 +14,103 @@
     </div>
     <div class="main-container">
       <task-details-menu @openModal="openModal" />
-      <component
+      <!-- <component
         @addLabel="addLabel"
         class="dynamic-details"
         :board="board"
         :is="'label-cmp'"
-      />
-      <task-details-menu @openModal="openModal" />
-      <component :is="'calendar-cmp'" @saveDate="saveDate" />
+      /> -->
+      <!-- <task-details-menu @openModal="openModal" /> -->
+      <!-- <component :is="'calendar-cmp'" @saveDate="saveDate" /> -->
     </div>
   </section>
 </template>
 
 <script>
-  import { taskService } from "../services/task.service";
-  import taskDetailsMenu from "../components/task-details-menu.vue";
-  import labelCmp from "./dynamic-components/label-cmp.vue";
-  import memberCmp from "./dynamic-components/member-cmp.vue";
-  import checklistCmp from "./dynamic-components/checklist-cmp.vue";
-  import calendarCmp from "./dynamic-components/calendar-cmp.vue";
+import { taskService } from "../services/task.service";
+import taskDetailsMenu from "../components/task-details-menu.vue";
+import labelCmp from "./dynamic-components/label-cmp.vue";
+import memberCmp from "./dynamic-components/member-cmp.vue";
+import checklistCmp from "./dynamic-components/checklist-cmp.vue";
+import calendarCmp from "./dynamic-components/calendar-cmp.vue";
 
-  export default {
-    props: {
-      taskId: {
-        type: String,
-        required: true,
-      },
-      groupId: {
-        type: String,
-        required: true,
-      },
-      boardId: {
-        type: String,
-        required: true,
-      },
+export default {
+  props: {
+    taskId: {
+      type: String,
+      required: true,
     },
-    data() {
-      return {
-        taskToEdit: null,
-        group: null,
-      };
+    groupId: {
+      type: String,
+      required: true,
     },
-    methods: {
-      // openModal(type) {
+    boardId: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      taskToEdit: null,
+      group: null,
+    };
+  },
+  methods: {
+    openModal(type) {},
+    closeTaskDetails() {
+      this.$emit("closeTaskDetails");
+    },
+    joinTask() {},
+    changeLabel() {},
+    makeChecklist() {},
+    addDate() {},
+    addAttachment() {},
+    changeCover() {},
+    copyTask() {},
+    archiveTask() {},
 
-      // },
-      closeTaskDetails() {
-        this.$emit("closeTaskDetails");
-      },
-      joinTask() {},
-      changeLabel() {},
-      makeChecklist() {},
-      addDate() {},
-      addAttachment() {},
-      changeCover() {},
-      copyTask() {},
-      archiveTask() {},
-
-       async saveDate(date) {
-        this.$emit("saveDate", date);
-      },
-
-      async addLabel(label) {
-        const idx = this.taskToEdit.labels.findIndex(
-          (lbl) => lbl.id === label.id
-        );
-        console.log(idx);
-        if (idx === -1) {
-          this.taskToEdit.labels.unshift(label);
-        } else {
-          this.taskToEdit.labels.splice(idx, 1);
-        }
-        const task = await this.$store.dispatch({
-          type: "updateTask",
-          task: JSON.parse(JSON.stringify(this.taskToEdit)),
-          groupId: this.groupId,
-        });
-        this.closeTaskDetails();
-      },
+    async saveDate(date) {
+      this.$emit("saveDate", date);
     },
-    computed: {
-      board() {
-        return this.$store.getters.board;
-      },
-    },
-    components: {
-      taskDetailsMenu,
-      labelCmp,
-      memberCmp,
-      checklistCmp,
-      calendarCmp
 
-    },
-    async created() {
-      const res = await taskService.getById(
-        this.taskId,
-        this.groupId,
-        this.boardId
+    async addLabel(label) {
+      const idx = this.taskToEdit.labels.findIndex(
+        (lbl) => lbl.id === label.id
       );
-      this.taskToEdit = { ...res.task };
-      this.group = { ...res.group };
+      // console.log(idx);
+      if (idx === -1) {
+        this.taskToEdit.labels.unshift(label);
+      } else {
+        this.taskToEdit.labels.splice(idx, 1);
+      }
+      const task = await this.$store.dispatch({
+        type: "updateTask",
+        task: JSON.parse(JSON.stringify(this.taskToEdit)),
+        groupId: this.groupId,
+      });
+      this.closeTaskDetails();
     },
-  };
+  },
+  computed: {
+    board() {
+      return this.$store.getters.board;
+    },
+  },
+  components: {
+    taskDetailsMenu,
+    labelCmp,
+    memberCmp,
+    checklistCmp,
+    calendarCmp,
+  },
+  async created() {
+    const res = await taskService.getById(
+      this.taskId,
+      this.groupId,
+      this.boardId
+    );
+    this.taskToEdit = { ...res.task };
+    this.group = { ...res.group };
+  },
+};
 </script>
