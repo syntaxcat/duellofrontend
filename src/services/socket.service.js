970e397b0919b8cb2c5f27b31085d54cb2528563
shemer binner <shemer146@gmail.com>
2022-03-30 17:17:09 +0300

    sockets

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? '' : '//localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? '' : '//localhost:3030';
 export const socketService = createSocketService();
 
 window.socketService = socketService;
