23ddb700158c7f4786a9538df6b13641daeae65c
shemer binner <shemer146@gmail.com>
2022-04-01 20:42:22 +0300

    home-page starred

--- src/components/all-boards.vue
+++ src/components/all-boards.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="all-boards">
-    <div class="starred" v-if="!boards">
+    <div class="starred" v-if="boards">
       <div class="all-board-header">
         <icon-base iconName="star" class="board-icon" />
         <h3>Starred boards</h3>
@@ -8,9 +8,27 @@
 
       <div class="boards-container">
         <ul>
-          <!-- <li v-for="board in boards" :key="board._id">
-              <a :href="'/board/' + board._id" class="board-title"></a>
-          </li>-->
+          <li v-for="board in starredBoards" :key="board._id">
+            <a
+              :href="'/#/board/' + board._id"
+              class="board-title full-star"
+              :style="
+                board.style.type === 'img'
+                  ? `background-image: url(${board.style.backgroundImg})`
+                  : `background-color:${board.style.color}`
+              "
+            >
+              <span class="board-title-fade"></span>
+
+              <div class="board-title-content">
+                <div class="title">{{ board.title }}</div>
+                <div class="container">
+                  <span>{{ board.subName }}</span>
+                  <icon-base iconName="starS" class="board-title-icon" @click.prevent="toggleFavorites(board)" />
+                </div>
+              </div>
+            </a>
+          </li>
         </ul>
       </div>
     </div>
@@ -23,11 +41,15 @@
 
       <div class="boards-container">
         <ul>
-          <li v-for="board in boards" :key="board._id">
+          <li v-for="board in unStarredBoards" :key="board._id">
             <a
               :href="'/#/board/' + board._id"
               class="board-title"
-              :style="(board.style.type === 'img') ? `background-image: url(${board.style.backgroundImg})` : `background-color:${board.style.color}`"
+              :style="
+                board.style.type === 'img'
+                  ? `background-image: url(${board.style.backgroundImg})`
+                  : `background-color:${board.style.color}`
+              "
             >
               <span class="board-title-fade"></span>
 
@@ -35,7 +57,7 @@
                 <div class="title">{{ board.title }}</div>
                 <div class="container">
                   <span>{{ board.subName }}</span>
-                  <icon-base iconName="starS" class="board-title-icon" />
+                  <icon-base iconName="starS" class="board-title-icon" @click.prevent="toggleFavorites(board)" />
                 </div>
               </div>
             </a>
@@ -92,7 +114,11 @@
             <a
               :href="'/#/board/' + board._id"
               class="board-title"
-              :style="(board.style.type === 'img') ? `background-image: url(${board.style.backgroundImg})` : `background-color:${board.style.color}`"
+              :style="
+                board.style.type === 'img'
+                  ? `background-image: url(${board.style.backgroundImg})`
+                  : `background-color:${board.style.color}`
+              "
             >
               <span class="board-title-fade"></span>
               <div class="board-title-content">
@@ -135,20 +161,26 @@ export default {
       required: true,
     },
   },
+  created() {
+    console.log(this.boards);
+  },
   methods: {
-    // toggleFavorites() {
-    //   this.board.isStarred = !this.board.isStarred
-    //   this.$store.dispatch({ type: 'updateBoard', board: this.board })
-    // }
+    toggleFavorites(board) {
+      board.isStarred = !board.isStarred;
+      this.$store.dispatch({ type: 'updateBoard', board });
+    },
   },
   computed: {
-
-    // isFavorite() {
-    //   console.log(this.board.isStarred)
-    //   if (this.board.isStarred) return 'full-star'
-    //   else return 'empty-star'
-    //   // return this.isFull ? 'full-star' : 'empty-star';
-    // },
+    isFavorite() {
+      // console.log(this.board.isStarred);
+      return this.board.isStarred ? 'full-star' : 'empty-star';
+    },
+    starredBoards() {
+      return this.boards.filter((board) => board.isStarred);
+    },
+    unStarredBoards() {
+      return this.boards.filter((board) => !board.isStarred);
+    },
   },
   components: { iconBase },
 };
