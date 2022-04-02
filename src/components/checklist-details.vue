bc6b99e83337429452997b9b98c9811a89da1990
Michaela Bereza <michaelabereza@icloud.com>
2022-04-02 16:58:11 +0300

    resizable textarea

--- src/components/checklist-details.vue
+++ src/components/checklist-details.vue
@@ -78,12 +78,7 @@
 
           <div class="add-todo edit-todo" v-if="isEdit && todo.id === target">
             <!-- <textarea  v-model="todo.title"></textarea> -->
-            <resizable-textarea
-              :placeholder="'Add an item'"
-              :autofocus="true"
-              :value="todo.title"
-              @valueChange="handleTodoValueChange"
-            />
+            <resizable-textarea :autofocus="true" :value="todo.title" @valueChange="(value) => (todo.title = value)" />
 
             <div class="add-controls">
               <div class="container">
@@ -123,7 +118,13 @@
 
     <div class="add-todo">
       <button v-if="!isAdd" @click="isAdd = !isAdd">Add an item</button>
-      <textarea placeholder="Add an item" v-if="isAdd" v-model="todoToAdd.title"></textarea>
+      <resizable-textarea
+        v-if="isAdd"
+        :placeholder="'Add an item'"
+        :autofocus="true"
+        :value="todoToAdd.title"
+        @valueChange="(value) => (todoToAdd.title = value)"
+      />
 
       <div class="add-controls" v-if="isAdd">
         <div class="container">
@@ -156,7 +157,7 @@ import { VueDraggableNext } from 'vue-draggable-next';
 import iconBase from './icon-base.vue';
 import deleteTodo from './dynamic-components/delete-todo-cmp.vue';
 import deleteModal from './delete-modal.vue';
-import ResizableTextarea from './resizable-textarea.vue';
+import resizableTextarea from './resizable-textarea.vue';
 
 export default {
   props: {
@@ -186,9 +187,6 @@ export default {
     });
   },
   methods: {
-    handleTodoValueChange(value) {
-      this.todo.title = value;
-    },
     toggleEdit(todoId) {
       // console.log('edit');
       if (this.target) {
@@ -264,7 +262,7 @@ export default {
     deleteTodo,
     deleteModal,
     draggable: VueDraggableNext,
-    ResizableTextarea,
+    resizableTextarea,
   },
 };
 </script>
