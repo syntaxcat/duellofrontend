12eeae73683fd74591eabc5255b70ea2b47d9ccc
Michaela Bereza <michaelabereza@icloud.com>
2022-08-22 23:29:45 +0300

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    publicPath: process.env.NODE_ENV === "production" ? "/duello/" : "/",
+    publicPath: process.env.NODE_ENV === 'production' ? '/duello/' : '/',
   },
   build: {
-    outDir: '../backend/public',
-    emptyOutDir: true
-  }
-})
+    outDir: '../duellobackend/public',
+    emptyOutDir: true,
+  },
+});
